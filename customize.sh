3365cdd2fc204cc8a80657f59441afadf59f11ba
coldnw <142607170+coldnw@users.noreply.github.com>
2024-12-19 09:33:36 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,1 +1,10 @@
-
+mkdir -p "$MODPATH/system/etc"
+mkdir -p "/sdcard/.config"
+cp "/system/etc/mkshrc" "$MODPATH/system/etc/mkshrc"
+echo 'CM="/sdcard/.config/shrc"
+if [[ -r "$CM" ]]; then
+    source "$CM"
+fi' >> "$MODPATH/system/etc/mkshrc"
+cp "$MODPATH/shrc" "/sdcard/.config"
+set_perm_recursive "$MODPATH" 0 0 0755 0644
+ui_print " Installation Completed "

5cf090c3bcb2e94f0b91d69d416aa8da559b1a79
StupidThingsAndroid <142607170+coldnw@users.noreply.github.com>
2024-12-19 11:24:06 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,11 +1,14 @@
 mkdir -p "$MODPATH/system/etc"
 mkdir -p "/sdcard/.config"
 cp "/system/etc/mkshrc" "$MODPATH/system/etc/mkshrc"
-echo 'CM="/sdcard/.config/shrc"
+MKSHRC_LINES='CM="/sdcard/.config/shrc"
 if [[ -r "$CM" ]]; then
     source "$CM"
-fi' >> "$MODPATH/system/etc/mkshrc"
+fi'
+if ! grep -Fq 'CM="/sdcard/.config/shrc"' "$MODPATH/system/etc/mkshrc"; then
+    echo "$MKSHRC_LINES" >> "$MODPATH/system/etc/mkshrc"
+fi
 cp "$MODPATH/shrc" "/sdcard/.config"
 set_perm_recursive "$MODPATH" 0 0 0755 0644
 rm "$MODPATH/shrc"
-ui_print " Installation Completed "
+ui_print "Installation Completed"
